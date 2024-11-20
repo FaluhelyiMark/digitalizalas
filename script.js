3c47142f9107144fc9beded205bbcf3ff34ec5f5
FaluhelyiMark <mark20faluhelyi@gmail.com>
2024-11-20 19:13:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles(); // Automatikusan az index.html-t használja
+app.UseStaticFiles();  // Statikus fájlok engedélyezése
+
+app.Run();
